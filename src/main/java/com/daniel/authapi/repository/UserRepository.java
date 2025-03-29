caca9c80f3d33e975f647c4ba6024bf789de04ea
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-03-29 07:49:03 -0300

    :sparkles: feat: Adiciona camada de persistencia de dados

---
+++ src/main/java/com/daniel/authapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.daniel.authapi.repository;
+
+import com.daniel.authapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+}
